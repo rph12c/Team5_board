c84e0a1118b7db1f00fe78e0edf144416917ca34
Sakari_the_machine_maker <harrisryanfe@gmail.com>
2020-09-28 16:19:30 -0400

    Assignment 6 and an interactive test scene

---
+++ Assets/Prefabs/Class/Temp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{	
+	public float speed;
+	float x,y;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		x = Input.GetAxis("Horizontal");
+		y = Input.GetAxis ("Vertical");
+        //transform.position = transform.position + new Vector3(x*speed, y*speed, 0f);
+		transform.position += new Vector3(x*speed, y*speed, 0f);
+    }
+}
