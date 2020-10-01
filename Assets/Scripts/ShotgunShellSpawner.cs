99a7f6570de329be7054819579928a247e1fbfe2
Cameron Baugh <C2.baugh@gmail.com>
2020-10-01 14:57:14 -0400

    (Trying this again) Added shotgun shell falling function.

---
+++ Assets/Scripts/ShotgunShellSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunShellSpawner : MonoBehaviour
+{
+
+    public GameObject shellPrefab;
+    public float generationTime = 0.4f;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(timedCreation());
+    }
+
+    private void spawn()
+    {
+        print("spawn");
+        GameObject s = Instantiate(shellPrefab) as GameObject;
+        s.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * 2);
+    }
+
+    IEnumerator timedCreation()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(generationTime);
+            spawn();
+        }
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
