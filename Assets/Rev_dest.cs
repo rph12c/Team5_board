edd4a09ff070caaa9ccea9b4109bd230611e29d0
Sakari_the_machine_maker <harrisryanfe@gmail.com>
2020-10-01 21:42:45 -0400

    Adding the other two bullets to the falling stuff

---
+++ Assets/Rev_dest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rev_dest : MonoBehaviour
+{
+
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //print(screenBounds.y);
+        //print("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(transform.position.y);
+        if (transform.position.y < -10)
+        {
+            print("destroyed");
+            Destroy(this.gameObject);
+
+        }
+    }
+}
