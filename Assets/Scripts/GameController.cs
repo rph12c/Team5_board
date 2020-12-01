0a8b18f28325ca1fcca42ef49fb5bbe560c28649
Cameron Baugh <C2.baugh@gmail.com>
2020-12-01 11:22:50 -0500

    First set of board mechanics.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameMode { SelectTerritory, SelectAdjacent };
+public enum PlayerTurn { Attack, Defense };
+
+public class GameController : MonoBehaviour
+{
+    
+    public GameMode gameMode;
+    public GameObject selectedSpace;
+    private GameObject[] currentAdjSpaces;
+
+    private static GameController sharedController;
+    public static GameController SharedController
+    {
+        get
+        {
+            if(sharedController == null)
+            {
+                GameObject go = new GameObject("GameController");
+                go.AddComponent<GameController>();
+            }
+            return sharedController;
+        }
+
+    }
+
+    
+    public PlayerTurn playerTurn;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        sharedController = this;
+    }
+
+    void Start()
+    {
+
+        sharedController = this;
+
+        //print("Begin");
+        gameMode = GameMode.SelectTerritory;
+        playerTurn = PlayerTurn.Attack;
+        selectedSpace = null;
+
+    }
+
+    // Update is called once per frame
+
+    private bool didStartParticles = false;
+    private bool didStopParticles = false;
+    void Update()
+    {
+        switch(gameMode)
+        {
+            case GameMode.SelectTerritory:
+
+                didStartParticles = false; //reset bool
+
+                if (selectedSpace != null && !didStopParticles)
+                {
+                    didStopParticles = true;
+                    selectedSpace = null;
+                    foreach (GameObject space in currentAdjSpaces)
+                    {
+                        space.GetComponent<TerritoryHandler>().stopParticle();
+                    }
+                }
+                break;
+            case GameMode.SelectAdjacent:
+
+                didStopParticles = false; //reset bool
+
+                if (selectedSpace != null && !didStartParticles)
+                {
+                    didStartParticles = true;
+                    print("should start particles");
+                    currentAdjSpaces = selectedSpace.GetComponent<TerritoryHandler>().adjacentTerritories;
+                    foreach (GameObject space in selectedSpace.GetComponent<TerritoryHandler>().adjacentTerritories)
+                    {
+                        space.GetComponent<TerritoryHandler>().playParticle();
+                    }
+                }
+                break;
+        }
+    }
+}
--- Assets/Scripts/TerritoryHandler.cs
+++ Assets/Scripts/TerritoryHandler.cs
@@ -9,14 +9,18 @@ public class TerritoryHandler : MonoBehaviour
 
     private SpriteRenderer sprite;
     private ParticleSystem particle;
+    private GameController controller;
 
     public Color32 oldColor;
     public Color32 startColor;
     public Color32 hoverColor;
+    public GameObject[] adjacentTerritories;
 
     // Start is called before the first frame update
     void Awake()
     {
+        controller = GameController.SharedController;
+
         sprite = GetComponent<SpriteRenderer>();
         particle = GetComponent<ParticleSystem>();
         particle.Stop();
@@ -25,14 +29,76 @@ public class TerritoryHandler : MonoBehaviour
 
     void OnMouseEnter()
     {
-        oldColor = sprite.color;
-        sprite.color = hoverColor;
-        particle.Play();
+        print("enter");
+        switch (controller.gameMode)
+        {
+            case GameMode.SelectTerritory:
+                oldColor = sprite.color;
+                sprite.color = hoverColor;
+                particle.Play();
+                break;
+            case GameMode.SelectAdjacent:
+                oldColor = sprite.color;
+                sprite.color = hoverColor;
+                break;
+        }
+        //print(controller.gameMode);
+        //switch (controller.gameMode)
+        //{
+        //    default:
+        //        oldColor = sprite.color;
+        //        sprite.color = hoverColor;
+        //        particle.Play();
+        //        break;
+        //}
     }
 
     void OnMouseExit()
     {
-        sprite.color = oldColor;
+        //print("exit");
+        switch (controller.gameMode)
+        {
+            case GameMode.SelectTerritory:
+                sprite.color = oldColor;
+                particle.Stop();
+                break;
+            case GameMode.SelectAdjacent:
+                sprite.color = oldColor;
+                break;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //print("click");
+        print(controller.gameMode);
+
+        switch (controller.gameMode)
+        {
+            case GameMode.SelectTerritory:
+                controller.selectedSpace = this.gameObject;
+                controller.gameMode = GameMode.SelectAdjacent;
+                //oldColor already set from hover
+                sprite.color = hoverColor;
+                break;
+            case GameMode.SelectAdjacent:
+                if(controller.selectedSpace == this.gameObject) //if click again to deselect
+                {
+                    controller.gameMode = GameMode.SelectTerritory;
+                    sprite.color = oldColor;
+                    particle.Stop();
+                }
+                break;
+        }
+    }
+
+    public void playParticle()
+    {
+        particle.Play();
+    }
+
+    public void stopParticle()
+    {
         particle.Stop();
     }
 }
