faf5c0f8ab296d7cae651faf1684792696206c73
mareklis8 <70965418+mareklis8@users.noreply.github.com>
2020-12-01 14:03:35 -0500

    DontDestroy Script for music

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroy : MonoBehavior { 
+
+    void Awake ()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MainMusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(ths.gameObject);
+    }
+}
