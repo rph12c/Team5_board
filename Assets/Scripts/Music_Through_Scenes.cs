4fac92e4709506c450511857c003366753d84158
mareklis8 <70965418+mareklis8@users.noreply.github.com>
2020-12-01 13:34:45 -0500

    Music control through different scenes

---
+++ Assets/Scripts/Music_Through_Scenes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MusicClass
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<_audioSource>();
+    }
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
+        
+    
+
