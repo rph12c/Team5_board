fbf3b36c36936439439108609441298c3f0c0687
Marek Lis <mareklis08@gmail.com>
2020-12-02 02:05:08 -0500

    functional settings menu

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DontDestroy : MonoBehaviour
+{
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MainMusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+
---
+++ MixerLevels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMasterLvl(float masterLvl)
+    {
+        audioMixer.SetFloat("MasterVolume", masterLvl);
+    }
+
+    public void SetMusicLvl(float musicLvl)
+    {
+        audioMixer.SetFloat("MainMusicVolume", musicLvl);
+    }
+
+    public void SetSfxLvl(float sfxLvl)
+    {
+        audioMixer.SetFloat("SFX", sfxLvl);
+    }
+}
