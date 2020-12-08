acf2e5b3a369bd3bf613765081c751d8adb8fc88
Marek Lis <mareklis08@gmail.com>
2020-12-08 05:34:08 -0500

    no message

---
+++ Assets/PlayBoardMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBoardMusic : MonoBehaviour
+{
+    AudioSource MainMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMusic.Stop();   
+    }
+
+    // Update is called once per frame
+    // void Update()
+}
